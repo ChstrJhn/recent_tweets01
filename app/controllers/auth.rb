6a2cc40949111f1a81c83a8f9f1eddace8968fec
Chester John <chesterjohn@Chesters-MacBook-Pro.local>
2015-09-08 18:18:23 +0800

    add AJAX

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -11,11 +11,11 @@ end
 get '/recent_tweets/:twitter_handle' do
 	@user = User.find_by(twitter_handle: params[:twitter_handle])
 	if @user.tweets_stale?
-	@user.tweets.each {|tweet| tweet.destroy}
-	@recent_tweets = @user.recent_tweets
-	erb :user_tweets
+	  @user.tweets.each {|tweet| tweet.destroy}
+	  @recent_tweets = @user.recent_tweets
+	  erb :user_tweets
     else
-	@recent_tweets = @user.tweets
-	erb :user_tweets
+	  @recent_tweets = @user.tweets
+	  erb :user_tweets
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,49 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$( ".recent_tweets_form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  $("#user_tweets").html("<img src='img/loading.gif'/>");
+  var button = $(this).children().find("#recent_tweets_btn")
+  button.val("Just a sec!");
+  button.attr("disabled",true);
+
+  $.ajax({
+  	url: $(this).attr("action"),
+  	method: $(this).attr("method"),
+  	data: $(this).serialize()
+  }).done(function(response){
+  	  console.log(response);
+  	  $("#user_tweets").children("img").remove();
+      $("#user_tweets").append(response);
+      button.val("Get Recent Tweets!");
+      button.attr("disabled",false);
+  	});
+  });
+
+
+
+
+
+// $("#recent_tweets_btn").submit(function(event){
+// event.preventDefault();
+// console.log("hi");
+// });
+
+// $('body').on('click', "#recent_tweets_btn", function(event){
+// event.preventDefault();
+// console.log("hi");
+// $(this).val("Just a sec!");
+// $(this).attr("disabled", "true");
+// });
+
+// $.ajax({
+//     url:"/recent_tweets",
+//     type:"GET",
+//     data: params,
+//     beforeSend: function() {
+//         alert("loading")
+//     },
+//     success:function(html){ alert(html) }
+// });
+
 });
